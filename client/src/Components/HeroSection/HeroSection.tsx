eb1e1ad4e517958883d2195578939cf16c4ea78a
s1nnzz <24095274@activatelearning.ac.uk>
2025-10-31 21:15:25 +0000

    added search bar and sign in styling and gradient thing

--- client/src/Components/HeroSection/HeroSection.tsx
+++ client/src/Components/HeroSection/HeroSection.tsx
@@ -1,7 +1,7 @@
 function HeroSection() {
 	return (
-		<div className="hero-new__background-image">
-			<picture className="hero-new__image">
+		<div className="hero">
+			<picture className="hero-newimage">
 				<source
 					media="(min-width: 760px)"
 					srcSet="https://www.riotgames.com/darkroom/2000/5bccfe853fd74c77bce2100588c82027:63d6d7b83f53690de6a3282523d24457/wrld25-homepage-herobanner-3840x1600-10-25.png"
